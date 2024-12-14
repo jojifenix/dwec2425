880e006ee3998536b778c6ffcc3787b5ca16a873
Jordi <160190573+jojifenix@users.noreply.github.com>
2024-12-14 22:03:42 +0100

    Delete Ejercicios_clase_1ºeva directory

--- Ejercicios_clase_1ºeva/tema1/ejem_var_concat.js
+++
@@ -1,8 +0,0 @@
-var nombre;
-var nombre= "Juan";
-var nombre="Juan", apellido='García', apellido2="López"
-var nombreApellido="Juan" + "" + "García";
-var nombreApellido= nombre + ""+ apellido;
-var edad=nacimiento + 32;
-var numero =23
-var c = a + b
